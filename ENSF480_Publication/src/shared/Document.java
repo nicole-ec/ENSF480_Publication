2479b9e71fb8a50b0027133029837daba6b1dd13
yashharanb <37969217+yashharanb@users.noreply.github.com>
2018-11-25 15:29:32 -0700

    Update Document.java

--- ENSF480_Publication/src/shared/Document.java
+++ ENSF480_Publication/src/shared/Document.java
@@ -4,6 +4,7 @@ public class Document {
 	private long isbn;
 	private String title;
 	private String authorName;
+	private double price;
 	
 	public Document (int isbn, String title, String authorName) {
 		this.isbn = isbn;
