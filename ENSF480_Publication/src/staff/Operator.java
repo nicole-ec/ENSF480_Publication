11c79b490b97b98fd636cb5597bbcc6f35e37f3b
Yashharan Babra <yashharanb@gmail.com>
2018-11-26 13:29:28 -0700

    update operator

--- ENSF480_Publication/src/staff/Operator.java
+++ ENSF480_Publication/src/staff/Operator.java
@@ -1,5 +1,13 @@
 package staff;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 
 import buyers.Order;
@@ -9,38 +17,39 @@ import shared.Inventory;
 
 public class Operator extends Staff{
 		
-	Inventory inventory;
-
 	public Operator(String username, String password) {
 		super(username,password);
 	}
 	
-	public Inventory addDoc(Document doc) {
-		inventory.add(doc);
-		return null;
-		
+	public void addDoc(Document doc) throws IOException {
+		Writer output = new BufferedWriter(new FileWriter("documents.txt", true));
+		output.append("\n"+doc.getIsbn()+";"+doc.getTitle()+";"+doc.getAuthor()+";"+doc.getPrice());
+		output.close();
 	}
 	
-	public Inventory removeDoc(int isbn) {
-		
-		for(int i = 0; i<inventory.getList().size(); i++)
-		{
-			if(inventory.getItem(i).getIsbn() == isbn)
-			{
-				inventory.getList().remove(i);
-			}
+	public void removeDoc(int isbn) throws IOException {
+		File fileName = new File("documents.txt");
+		File tempFile = new File("documents1.txt");
+
+		BufferedReader reader = new BufferedReader(new FileReader(fileName));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+		String lineToRemove = Integer.toString(isbn);
+		String currentLine;
+
+		while((currentLine = reader.readLine()) != null) {
+		    String trimmedLine = currentLine.trim();
+		    if(trimmedLine.contains(lineToRemove)) continue;
+		    writer.write(currentLine + System.getProperty("line.separator"));
 		}
-		return null;
-		
+		writer.close(); 
+		reader.close();
+		fileName.delete();
+		tempFile.renameTo(fileName);
 	}
 	
-	public Inventory updateDoc(ArrayList<Document> arr) {
-	
-		for(int i = 0; i<inventory.getList().size();i++)
-		{
-			inventory.getList().set(i,arr.get(i));
-		}
-		return null;
-	
+	public void updateDoc(Document doc) throws IOException {
+		removeDoc((int)doc.getIsbn());
+		addDoc(doc);		
 	}
 }
