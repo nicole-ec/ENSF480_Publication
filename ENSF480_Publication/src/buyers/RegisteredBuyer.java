1664593730c383062603731148d023728c6eec30
Yashharan Babra <yashharanb@gmail.com>
2018-11-25 18:30:34 -0700

    update registered buyer

--- ENSF480_Publication/src/buyers/RegisteredBuyer.java
+++ ENSF480_Publication/src/buyers/RegisteredBuyer.java
@@ -1,5 +1,11 @@
 package buyers;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -16,7 +22,7 @@ public class RegisteredBuyer extends Buyer{
 	private String password;
 	private ArrayList<Promotion> promo;
 	
-	public RegisteredBuyer(String email, String creditCard, String username, String password, ArrayList<Promotion> promo) {
+	public RegisteredBuyer(String username, String password, String email, String creditCard, ArrayList<Promotion> promo) {
 		super();
 		isSubscribed = true;
 		this.email = email;
@@ -26,29 +32,42 @@ public class RegisteredBuyer extends Buyer{
 		this.promo = promo;
 	}
 	
-	public void unsubscribe() {
+	public void unsubscribe() throws IOException {
 		isSubscribed = false; 
-		String fileName = "login.txt";
-		String lineToRemove = "username";	
-		try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-			stream.filter(line->!line.trim().equals(lineToRemove)).forEach(System.out::println);
-		} catch (IOException e) {
-			e.printStackTrace();
+				
+		File fileName = new File("login.txt");
+		File tempFile = new File("login1.txt");
+
+		BufferedReader reader = new BufferedReader(new FileReader(fileName));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+		String lineToRemove = username;
+		String currentLine;
+
+		while((currentLine = reader.readLine()) != null) {
+		    // trim newline when comparing with lineToRemove
+		    String trimmedLine = currentLine.trim();
+		    if(trimmedLine.contains(lineToRemove)) continue;
+		    writer.write(currentLine + System.getProperty("line.separator"));
 		}
+		writer.close(); 
+		reader.close();
+		fileName.delete();
+		tempFile.renameTo(fileName);
 	}
 	
 	public void addPromo(Promotion p) {
 		promo.add(p);
 	}
-	
+
 	@Override
-	public Document searchCatalog() {
+	public Document searchCatalog(String title) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Order makeOrder() {
+	public Order makeOrder(Document d) {
 		// TODO Auto-generated method stub
 		return null;
 	}
